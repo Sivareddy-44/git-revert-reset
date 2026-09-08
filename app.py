0d431c8163378ed9ad65b82dd83b99406cfc74c8
Siva Reddy <yennegivenkatasivareddy44@gmail.com>
2026-09-08 19:35:39 +0530

    upd

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 num = int(input("Enter a number to check: "))
 
 # Check if the remainder is 0 when divided by 2
-if num % 2 == 0:
+if num % 3 == 0:
     print(f"{num} is an Even number.")
 else:
     print(f"{num} is an Odd number.")
