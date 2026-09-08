54e6e84f1ae0a64df4bbd087a94b3dca1322bc8d
yvsr44 <yennegivenkatasivareddy44@gmail.com>
2026-09-08 16:15:10 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,8 @@
+# Get a number from the user and convert it to an integer
+num = int(input("Enter a number to check: "))
+
+# Check if the remainder is 0 when divided by 2
+if num % 2 == 0:
+    print(f"{num} is an Even number.")
+else:
+    print(f"{num} is an Odd number.")
